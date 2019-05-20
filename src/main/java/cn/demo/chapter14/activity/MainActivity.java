2a24c0053dc47b116234f4657da626b62878bf87
zhangjianandroid <3273376405@qq.com>
2019-05-20 08:15:45 +0800

    第五次提交：修改一些类名

--- src/main/java/cn/demo/chapter14/activity/MainActivity.java
+++ src/main/java/cn/demo/chapter14/activity/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import cn.demo.chapter14.R;
 
 /**
- *项目名称：WeatherActivity.java
+ *项目名称：MainActivity.java
  *类描述： 将 省市县级别的碎片 添加到 Activity活动中去
  *创建人：ZJ
  *创建时间：2019/5/17 16:45
@@ -14,11 +14,11 @@ import cn.demo.chapter14.R;
  *修改备注：
  *@version
  */
-public class WeatherActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.weather_activity);
+        setContentView(R.layout.activity_main);
     }
 }
